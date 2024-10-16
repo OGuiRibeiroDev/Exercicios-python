ee4c13811e34f3b27b50587f6701c973da94ddfa
Guilherme Henrique Machado RIbeiro <100424449+Sh1inx@users.noreply.github.com>
2024-10-15 22:58:30 -0300

    Subindo exercicios feitos

--- curso_python/aula14.py
+++ curso_python/aula14.py
@@ -5,7 +5,7 @@ imc = peso / (altura ** 2)
 
 "f-strings"
 linha_1 = f'{nome} tem {altura:.2f} de altura'
-linha_2 = f'pesa {peso} e seu ICM é {imc:.2f}'
+linha_2 = f'pesa {peso} e seu ICM é'
 linha_3 = f'{imc:.2f}'
 
 print(linha_1)
