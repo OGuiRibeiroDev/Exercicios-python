567bacec16d686c11c5c5ccb30d63bdfa865aca4
OGuiRibeiroDev <guilherme.henrique2004@gmail.com>
2024-10-22 08:46:42 -0300

    Modulos

---
+++ curso_python/aula99.py
@@ -0,0 +1,16 @@
+# from sys import path
+
+# import aula99_package.modulo
+# from aula99_package import modulo
+# from aula99_package.modulo import *
+# # from aula99_package.modulo import soma_modulo
+
+# # print(*path, sep='\n')
+# print(soma_modulo(1, 2))
+# print(aula99_package.modulo.soma_modulo(1, 2))
+# print(modulo.soma_modulo(1, 2))
+# print(variavel)
+# print(nova_variavel)
+from aula99_package.modulo import soma_modulo
+
+print(__name__)
---
+++ curso_python/aula99_package/modulo.py
@@ -0,0 +1,15 @@
+# __all__ = [
+#     'variavel',
+#     'soma_modulo'
+#     'nova_variavel'
+# ]
+
+from modulo_b import fala_oi
+
+variavel = 'something'
+
+def soma_modulo(x, y):
+    return x + y
+
+nova_variavel = 'nada'
+fala_oi()
